272e91c7b4f202c98215f3cf1b7b95bfb70bb341
Vincent Manning <77985978+VincentManning518@users.noreply.github.com>
2022-11-17 15:34:21 -0500

    Add files via upload

---
+++ Interface (Version 1)/src/Quiz.java
@@ -0,0 +1,16 @@
+
+public class Quiz implements Measurable {
+
+	private double grade;
+	
+	public Quiz (double grade)
+	{
+		this.grade = grade;
+	}
+	
+	public double getMeasure() 
+	{
+		return grade;
+	}
+
+}
